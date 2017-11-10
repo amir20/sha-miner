2e6b6505db1135f34bec476ff9bf5d998caed886
Amir Raminfar <findamir@gmail.com>
2017-11-10 09:20:21 -0800

    Adds multiple threads

--- test.go
+++ test.go
@@ -6,38 +6,74 @@ import (
 	"encoding/binary"
 	"math/big"
 	"os"
-	"time"
+	"runtime"
+	metrics2 "github.com/rcrowley/go-metrics"
 	"fmt"
 )
 
-func main() {
-	bytes := argToBytes()
-	total := len(bytes)
+const (
+	maxUint64  = ^uint64(0)
+	difficulty = 100000000
+)
 
+var (
+	maxUint256 = new(big.Int).Exp(big.NewInt(2), big.NewInt(256), big.NewInt(0))
+	threshold  = new(big.Int).Div(maxUint256, big.NewInt(difficulty))
+)
+
+func main() {
+	abort := make(chan struct{})
+	found := make(chan uint64)
+	defer close(found)
+
+	data := argToBytes()
+	delta := maxUint64 / uint64(runtime.NumCPU())
+
+	metrics := metrics2.NewMeter()
+	for i := 0; i < runtime.NumCPU(); i++ {
+		start := uint64(i) * delta
+		go mine(start, data, found, abort, metrics)
+	}
+
+	select {
+	case result := <-found:
+		close(abort)
+		fmt.Println("Found result", result)
+		fmt.Printf("Effective Hashrate is %.2f MH/s\n", metrics.Rate1()/1000000)
+	}
+}
+
+func mine(start uint64, bytes []byte, found chan<- uint64, abort <-chan struct{}, meter metrics2.Meter) {
+	total := len(bytes)
 	buffer := make([]byte, total+8)
 	copy(buffer, bytes)
 
-	var maxUint256 = new(big.Int).Exp(big.NewInt(2), big.NewInt(256), big.NewInt(0))
-	threshold := new(big.Int).Div(maxUint256, big.NewInt(1000000000))
-
-	start := time.Now()
-	found := int64(0)
-	for i := int64(0); i < 1<<63-1; i++ {
-		binary.PutVarint(buffer[total:], i)
-		sum := sha256.Sum256(buffer)
-
-		value := new(big.Int).SetBytes(sum[:])
-
-		if value.Cmp(threshold) < 0 {
-			println("Found match", value.String())
-			found = i
+	nonce := start
+	attempt := int64(0)
+	for {
+		select {
+		case <-abort:
 			break
+
+		default:
+			binary.LittleEndian.PutUint64(buffer[total:], nonce)
+			sum := sha256.Sum256(buffer)
+			value := new(big.Int).SetBytes(sum[:])
+
+			if value.Cmp(threshold) <= 0 {
+				found <- nonce
+				break
+			}
+
+			nonce++
+			attempt++
+			if attempt % (1<<16) == 0 {
+				meter.Mark(attempt)
+				attempt = 0
+
+			}
 		}
 	}
-
-	hashesPerSecond := found / int64(time.Since(start).Seconds())
-
-	fmt.Printf("Found a match with hashrate of %.2f MH/s\n", float32(hashesPerSecond)/float32(1000000))
 }
 
 func argToBytes() []byte {
