235fc83234f7b0f95386edac5e46e88327bf401f
Amir Raminfar <findamir@gmail.com>
2017-11-11 14:31:57 -0800

    Adds comments

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// Copyright 2017 Amir Raminfar
+
 package main
 
 import (
@@ -23,26 +25,33 @@ var (
 )
 
 func main() {
+	// read the values from command line
 	difficulty := flag.Uint64P("difficulty", "d", defaultDifficulty, "Difficulty value to use for mining")
 	message := flag.StringP("message", "m", defaultMessage, "Message to compute the hash")
 	threads := flag.IntP("threads", "t", runtime.NumCPU(), "Total number of threads to use. Defaults to number of CPUs")
-
 	flag.Parse()
 
+	// total max uint divided by the difficulty determines the threshold. Bigger difficulty means smaller threshold
 	threshold = new(big.Int).Div(maxUint256, big.NewInt(int64(*difficulty)))
 	data := []byte(*message)
 
+	// create channels needed for results and abort
 	abort := make(chan struct{})
 	found := make(chan uint64)
 	defer close(found)
 
+	// split the total searchable space for nonce into equal chunks
 	delta := maxUint64 / uint64(*threads)
 	meter := metrics2.NewMeter()
+
 	for i := 0; i < *threads; i++ {
+
+		// start is the beginning of attempts for this thread which be 0, delta, 2 x delta, ...
 		start := uint64(i) * delta
 		go mine(start, data, found, abort, meter)
 	}
 
+	// create a new status thread
 	go status(abort, meter)
 
 	select {
@@ -52,23 +61,36 @@ func main() {
 	}
 }
 
+// mine is a worker routine that does the actual work for each thread
 func mine(start uint64, bytes []byte, found chan<- uint64, abort <-chan struct{}, meter metrics2.Meter) {
+
+	// create a copy of the bytes and add extra padding for int64 (8 bytes)
 	total := len(bytes)
 	buffer := make([]byte, total+8)
 	copy(buffer, bytes)
 
 	nonce := start
 	attempt := int64(0)
+
+
 	for {
 		select {
 		case <-abort:
+			// stop the worker and return
 			return
 
 		default:
+			// convert the nonce candidate to bytes and append to the end of buffer
+			// note that the buffer is reused here between each attempt by always overwriting the last 8 bytes
 			binary.LittleEndian.PutUint64(buffer[total:], nonce)
+
+			// compute the sha
 			sum := sha256.Sum256(buffer)
+
+			// convert the sha to big integer
 			value := new(big.Int).SetBytes(sum[:])
 
+			// if the integer value of the sha is less than the threshold then we have found a match
 			if value.Cmp(threshold) <= 0 {
 				found <- nonce
 				return
@@ -76,6 +98,7 @@ func mine(start uint64, bytes []byte, found chan<- uint64, abort <-chan struct{}
 
 			nonce++
 			attempt++
+			// update the meter every 2^16 for better performance. This is purely optimization I saw in geth
 			if attempt%(1<<16) == 0 {
 				meter.Mark(attempt)
 				attempt = 0
@@ -84,6 +107,7 @@ func mine(start uint64, bytes []byte, found chan<- uint64, abort <-chan struct{}
 	}
 }
 
+// status is a simple worker thread that prints rate of the meter every 5 seconds
 func status(abort <-chan struct{}, meter metrics2.Meter) {
 	ticker := time.NewTicker(5 * time.Second)
 	for {
